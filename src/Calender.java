05856c292b92393e27fa07405870f94320ace097
hyunjeong <dhdbs200@gmail.com>
2022-12-12 22:29:07 +0900

    Add getMaxDaysOfMonth

--- src/Calender.java
+++ src/Calender.java
@@ -1,11 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Calender {
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(" 일 월 화 수 목 금 토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 29 27 28");
+		
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		Calender cal = new Calender();
+
+		System.out.println(num + "월은 " + cal.getMaxDaysOfMonth(num) + "일까지 있습니다.");
+		
 	}
 }
